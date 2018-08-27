1f900bcfdcb5fcb32f3eda131a5cb08f62537f0a
Adrian Panella <ianchi74@outlook.com>
2018-08-27 08:47:39 -0500

    chore: update example app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,11 @@
  * https://opensource.org/licenses/MIT
  */
 
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { IWidgetDef } from 'reactive-json-form-ng';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +18,71 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  widgetDef: IWidgetDef;
+
+  constructor(private http: HttpClient) {
+    this.widgetDef = {
+      type: 'card',
+      options: {
+        title: 'Card Title',
+        description: 'this is a description',
+      },
+      content: [
+        {
+          type: 'form',
+          bind: 'data',
+
+          content: [
+            {
+              type: 'input',
+              bind: '$model.field1',
+              options: {
+                title: 'Input field',
+              },
+            },
+            {
+              type: 'form-array',
+              bind: '$model.children',
+              options: {
+                newRow: '{}',
+                allowDel: true,
+              },
+              content: [
+                {
+                  type: 'form',
+                  bind: '$data',
+                  content: [
+                    {
+                      type: 'input',
+                      bind: '$data.subfield1',
+                      options: {
+                        title: 'Row field1',
+                      },
+                    },
+                    {
+                      type: 'input',
+                      bind: '$data.subfield2',
+                      options: {
+                        title: 'Row field2',
+                      },
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              type: 'button',
+              bind: '$model.$click',
+              options: { title: 'Show data', click: 'true' },
+            },
+            {
+              type: 'code',
+              options: { 'text=': 'JSON.stringify($model.$click && $model,undefined,2)' },
+            },
+          ],
+        },
+      ],
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,61 @@
  * https://opensource.org/licenses/MIT
  */
 
+import {} from '@angular/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RxObject } from 'espression-rx';
+import {
+  CommonWidgetsModule,
+  Context,
+  expressionProvider,
+  FormFieldWidgetsModule,
+  ROOT_EXPR_CONTEXT,
+} from 'reactive-json-form-ng';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    CommonWidgetsModule,
+    HttpClientModule,
+    FormFieldWidgetsModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [
+    expressionProvider,
+    {
+      provide: ROOT_EXPR_CONTEXT,
+      useValue: Context.create(
+        undefined,
+        {
+          RxObject,
+          data: RxObject(
+            {
+              children: [
+                {
+                  subfield1: 'a',
+                  subfield2: 'b',
+                },
+                {
+                  subfield1: 'c',
+                  subfield2: 'f',
+                },
+              ],
+            },
+            true
+          ),
+        },
+        undefined,
+        undefined,
+        true
+      ),
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
