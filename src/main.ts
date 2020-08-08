4cdc2ec021e5bbd53ac83b4077d77baf2e2c0ea0
Adrian Panella <ianchi74@outlook.com>
2020-08-08 11:28:47 -0500

    app: remove jit compiler

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@
  */
 
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -15,6 +15,6 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
+platformBrowser()
   .bootstrapModule(AppModule)
   .catch((err) => console.log(err));
